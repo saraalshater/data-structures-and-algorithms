132f5dab055b7990e42cf35e3d276529b1a11c9d
saraalshater <gleave.alshater@gmail.com>
2021-10-15 22:20:31 +0300

    codechallange10 with test

--- java/staks-queue/lib/src/test/java/staks/queue/LibraryTest.java
+++ java/staks-queue/lib/src/test/java/staks/queue/LibraryTest.java
@@ -4,11 +4,120 @@
 package staks.queue;
 
 import org.junit.jupiter.api.Test;
+import staks.queue.structure.Queues;
+import staks.queue.structure.Stack;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LibraryTest {
-    @Test void someLibraryMethodReturnsTrue() {
-        Main classUnderTest = new Main();
-        assertTrue(classUnderTest.someLibraryMethod(), "someLibraryMethod should return 'true'");
-    }
+  @Test
+  public void pushTest() {
+    Stack nodeOne = new Stack();
+
+    nodeOne.push("SARA");
+
+    assertEquals("Stack{top=Node{data='SARA', next=null}}",nodeOne.toString());
+
+  }
+
+
+  @Test
+  public void popTest() {
+    Stack nodeOne = new Stack();
+
+    nodeOne.push("SARA");
+    nodeOne.push("SAR");
+    nodeOne.push("S");
+    nodeOne.pop();
+
+    assertEquals("Stack{top=Node{data='SAR', next=Node{data='SARA', next=null}}}",nodeOne.toString());
+
+  }
+  //
+  @Test
+  public void peekTest() {
+    Stack nodeOne = new Stack();
+
+    nodeOne.push("SARA");
+    nodeOne.push("SAR");
+    nodeOne.push("S");
+    nodeOne.peek();
+
+    assertEquals("S",nodeOne.peek());
+
+  }
+  @Test
+  public void checkIsEmpty() {
+    Stack nodeOne = new Stack();
+
+    nodeOne.push("SARA");
+    nodeOne.push("SAR");
+    nodeOne.push("S");
+    nodeOne.pop();
+    nodeOne.pop();
+    nodeOne.pop();
+
+//        assertTrue(true, nodeOne.isEmpty());
+
+  }
+  //
+//
+//
+  @Test
+  public void enqueueTest() {
+    Queues nodeTwo = new Queues();
+
+    nodeTwo.enqueue("SARA");
+    nodeTwo.enqueue("SAR");
+    nodeTwo.enqueue("S");
+
+    assertEquals("Queues{front=Node{data='SARA', next=Node{data='SAR', next=Node{data='S', next=null}}}, rear=Node{data='S', next=null}}",nodeTwo.toString());
+
+
+  }
+  //
+//
+  @Test
+  public void dequeueTest() {
+    Queues nodeTwo = new Queues();
+
+    nodeTwo.enqueue("SARA");
+    nodeTwo.enqueue("SAR");
+    nodeTwo.enqueue("S");
+    nodeTwo.dequeue();
+
+    assertEquals("SAR",nodeTwo.dequeue());
+
+
+  }
+  @Test
+  public void peekQueueTest() {
+    Queues nodeTwo = new Queues();
+
+    nodeTwo.enqueue("SARA");
+    nodeTwo.enqueue("SAR");
+    nodeTwo.enqueue("S");
+    nodeTwo.peek();
+
+    assertEquals("SARA",nodeTwo.peek());
+
+
+  }
+  @Test
+  public void queueIsEmptyTest() {
+    Queues nodeTwo = new Queues();
+
+    nodeTwo.enqueue("SARA");
+    nodeTwo.enqueue("SAR");
+    nodeTwo.enqueue("S");
+    nodeTwo.dequeue();
+    nodeTwo.dequeue();
+    nodeTwo.dequeue();
+
+    assertTrue(nodeTwo.isEmpty());
+
+
+  }
+
+
 }
